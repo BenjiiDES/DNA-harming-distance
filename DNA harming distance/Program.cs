5c7ca062ee8e3f0e1972a4a5d08e8bdf7aab641d
Benja <Benja@LAPTOP-I7718D1J>
2022-08-17 15:47:46 +0200

    Add project files.

---
+++ DNA harming distance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNA_harming_distance
+{
+    internal class Program
+    {
+       
+       // function to calculate
+        // Hamming distance
+        static int Hammingdist(String str1,
+                               String str2)
+        {
+            int i = 0, count = 0;
+            while (i < str1.Length)
+            {
+                if (str1[i] != str2[i])
+                    count++;
+                i++;
+            }
+            return count;
+        }
+
+       
+        public static void Main()
+        {
+            Console.WriteLine("din første string");
+            String str1 = Console.ReadLine();
+            Console.WriteLine("din anden string");
+            String str2 = Console.ReadLine();
+            
+                
+
+                Console.Write("der er ");
+                Console.Write(Hammingdist(str1, str2));
+                Console.Write(" harmin distance mellem de to DNA strings");
+                Console.ReadKey();
+            
+            
+        }
+    }
+}
